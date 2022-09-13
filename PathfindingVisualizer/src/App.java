8c04e453f5828b74bcd9e5074d0d4577b40fca8e
GuusLammers <guuslammers16@gmail.com>
2022-09-12 19:50:47 -0700

    Initial commit

---
+++ PathfindingVisualizer/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        Model model = new Model();
+        View view = new View();
+        new Controller(model, view);
+
+        view.setVisible(true);
+    }
+
+}
---
+++ PathfindingVisualizer/src/Controller.java
@@ -0,0 +1,11 @@
+public class Controller {
+
+    Model model;
+    View view;
+
+    Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
+
+}
