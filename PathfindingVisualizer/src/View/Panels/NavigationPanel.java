68b6358cc10c3f113bcc19e461a5919ea4944fed
GuusLammers <guuslammers16@gmail.com>
2022-09-12 20:15:18 -0700

    Added buttons and combobox to Panel

--- PathfindingVisualizer/src/View/Panels/NavigationPanel.java
+++ PathfindingVisualizer/src/View/Panels/NavigationPanel.java
@@ -1,13 +1,35 @@
 package View.Panels;
 
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 
 import java.awt.Color;
+import java.awt.GridLayout;
 
 public class NavigationPanel extends JPanel {
     
+    JButton startButton;
+    JButton clearButton;
+    JButton resetButton;
+
+    JComboBox<String> algorithmSelector;
+
     public NavigationPanel() {
-        this.setBackground(Color.green);
+        this.setLayout(new GridLayout());
+
+        this.startButton = new JButton("Start");
+        this.clearButton = new JButton("Clear");
+        this.resetButton = new JButton("Reset");
+
+        this.algorithmSelector = new JComboBox<String>();
+        this.algorithmSelector.addItem("Dijkstra's");
+        this.algorithmSelector.addItem("A*");
+
+        this.add(startButton);
+        this.add(algorithmSelector);
+        this.add(clearButton);
+        this.add(resetButton);
     }
 
 }
